8c0c56f7f8e25b89a9f022dd55a121efa74f17df
Tomi <258220040@qq.com>
2014-06-24 11:34:49 +0800

    ddd

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 requirejs.config({
 	//By default load any module IDs from js/lib
 	baseUrl:'js',
-	deps:['./lib/Fish','./lib/ejs_production', './lib/json2','./lib/jquery.qrcode','./lib/qrcode']
+	deps:['./lib/Fish', './lib/json2','./lib/jquery.qrcode','./lib/qrcode']
 //	,urlArgs: 'v=' + (new Date()).getTime()
 });
 
 // Start the main app logic.
 requirejs(
-	['./lib/Fish','./lib/ejs_production', './lib/json2','./lib/jquery.qrcode','./lib/qrcode', './app/index'],
+	['./lib/Fish', './lib/json2','./lib/jquery.qrcode','./lib/qrcode', './app/index'],
 	function () {
 		//jQuery, canvas and the app/sub module are all
 		//loaded and can be used here now.
